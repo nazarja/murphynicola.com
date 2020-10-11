e6e8970932729210675d17d843af9e3cfae881f0
Sean Murphy <mail.seanmurphy@gmail.com>
2020-10-11 13:24:47 +0100

    fixed responsive issue

--- murphynicola.com-backup/src/store/actions/actionTypes.js
+++
@@ -1,8 +0,0 @@
-export const GET_ALL_DATA =  'GET_ALL_DATA';
-export const UPDATE_ABOUT =  'UPDATE_ABOUT';
-export const UPDATE_LANDING =  'UPDATE_LANDING';
-export const SET_USER =  'SET_USER';
-export const CLEAR_USER =  'CLEAR_USER';
-export const SET_ADMIN_PAGE =  'SET_ADMIN_PAGE';
-export const DELETE_PROJECT = 'DELETE_PROJECT';
-export const UPDATE_PROJECT = 'UPDATE_PROJECT';
